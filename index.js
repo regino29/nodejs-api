8c0d294e0e2efd436a5b75a8d142f31e08df451b
regino29 <demegino@gmail.com>
2022-02-04 22:33:48 +0200

    password encryption

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { request } = require("express");
+//const { request } = require("express");
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 
 const users = [];
 
@@ -10,30 +11,35 @@ const { user } = new PrismaClient();
 app.use(express.json());
 
 app.listen(5000, () => {
-  console.log("geia sou xrhsto magka");
+  console.log("OK");
 });
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   const userExist = await user.findFirst({
-    where: { username, password },
+    where: { username },
     select: {
       id: true,
       username: true,
+      password: true,
     },
   });
-  console.log(userExist);
+
   if (!userExist) {
     return res.status(401).send("ekanes malakia");
   }
 
-  console.log(username, password);
-  res.status(200).send(userExist);
+  if (!(await bcrypt.compare(password, userExist.password))) {
+    return res.status(401).send("ekanes malakia");
+  }
+  res.status(200).send({ id: userExist.id, username: userExist.username });
 });
 
 app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
+  const hashedPass = await bcrypt.hash(req.body.password, 10);
+  const username = req.body.username;
+  //const { username, password } = req.body;
 
   const userExist = await user.findUnique({
     where: { username },
@@ -46,7 +52,7 @@ app.post("/register", async (req, res) => {
   const newUser = await user.create({
     data: {
       username,
-      password,
+      password: hashedPass,
     },
     select: {
       id: true,
